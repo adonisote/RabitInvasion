06451e432df3e69e2744ec5418efd178a039f75e
adonisote <adonis.almagro@code.berlin>
2023-10-30 23:46:46 +0100

    Pilatus1 as a space ship

--- code_invasion.py
+++ code_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 class AlienInvasion:
     '''Overall class to manage game assets and behavior.'''
     def __init__(self):
@@ -16,8 +18,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #Set the background color. 
-        #self.bg_color = (253, 223,89)
+        self.ship = Ship(self)
+
 
     def run_game(self):
         '''Start the main loop for the game.'''
@@ -28,6 +30,7 @@ class AlienInvasion:
                     sys.exit()
             #Redrwan the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             #Make the most recently drawn screen visible. 
             pygame.display.flip()
